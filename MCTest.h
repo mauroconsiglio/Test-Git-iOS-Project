a4f31b876118472c2ea30b6bcc53236f77d6f3db
Mauro Consiglio <Mauro@mauro-mac-mini.lan>
2014-08-09 22:20:22 +0200

    added a test class

---
+++ MCTest.h
@@ -0,0 +1,13 @@
+//
+//  MCTest.h
+//  Git Test
+//
+//  Created by Mauro Consiglio on 09/08/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCTest : NSObject
+
+@end
---
+++ MCTest.m
@@ -0,0 +1,13 @@
+//
+//  MCTest.m
+//  Git Test
+//
+//  Created by Mauro Consiglio on 09/08/14.
+//
+//
+
+#import "MCTest.h"
+
+@implementation MCTest
+
+@end
